c86109e77e776b356889b0890321e76d3b746e09
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2020-01-20 22:40:41 +0100

    Make handle_event an associated function

--- raiden/event_handler.rs
+++ raiden/event_handler.rs
@@ -4,17 +4,15 @@ use crate::service::RaidenService;
 pub struct EventHandler {}
 
 impl EventHandler {
-    pub fn new() -> Self {
-        EventHandler {}
-    }
-
-    pub async fn handle_event(&self, raiden: &RaidenService, event: Event) {
+    pub async fn handle_event(raiden: &RaidenService, event: Event) {
         match event {
             Event::TokenNetworkCreated(event) => {
                 let token_network_address = event.token_network.address;
-                raiden
-                    .contracts_registry
-                    .create_contract_event_filters("TokenNetwork".to_string(), token_network_address);
+                raiden.contracts_registry.create_contract_event_filters(
+                    "TokenNetwork".to_string(),
+                    token_network_address.into(),
+                    event.block_number.into(),
+                );
                 raiden.poll_filters().await;
             }
         }
